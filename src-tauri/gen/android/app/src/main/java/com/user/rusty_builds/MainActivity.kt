da14e6fb4a8e00a9883af38bb5b8f7b2e0b4396d
Zach Halstead <z.s.halstead@gmail.com>
2026-02-03 20:34:22 +1100

    init commit

---
+++ src-tauri/gen/android/app/src/main/java/com/user/rusty_builds/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.user.rusty_builds
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
